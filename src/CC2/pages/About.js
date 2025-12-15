5569886851be17e72d9cc221243e7ba56879c9e8
elghoulkhadija <elghoulkhadija7@gmail.com>
2025-12-15 20:26:42 +0100

    Delete src/CC2 directory
    delete

--- src/CC2/pages/About.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div className="p-4 max-w-xl mx-auto">
-      <h1 className="text-2xl font-bold mb-2">À propos de l'application</h1>
-      <p>Cette application permet de rechercher et d'afficher des films via l'API OMDb.</p>
-    </div>
-  );
-}
